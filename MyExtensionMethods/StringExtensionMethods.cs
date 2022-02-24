46558b43d7ecf44922d9a11d6e42244225324da6
Vilas Sagar <Vilas.Sagar@wolterskluwer.com>
2022-02-24 12:23:53 +0530

    Add project files.

---
+++ MyExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensionMethods
+{
+    public static class StringExtensionMethods
+    {
+        internal static string ReplaceFirstOccurrence(this string mainString, string pattern, string replace)
+        {
+            int pos = mainString.IndexOf(pattern);
+            if (pos < 0)
+            {
+                return mainString;
+            }
+            return mainString.Substring(0, pos) + replace + mainString.Substring(pos + pattern.Length);
+        }
+
+    }
+}
